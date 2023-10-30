75de83ea5536762ad273af8644c87d45f6856628
Ivan Tvisher <Tvisher@mail.ru>
2023-10-30 18:09:53 +0600

    small notebooks adaptive +

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ window.addEventListener('load', () => {
         debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
         throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
         // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-        offset: 120, // offset (in px) from the original trigger point
+        offset: 10, // offset (in px) from the original trigger point
         delay: 0, // values from 0 to 3000, with step 50ms
         duration: 1200, // values from 0 to 3000, with step 50ms
         easing: 'ease', // default easing for AOS animations

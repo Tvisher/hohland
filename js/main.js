a361286ef347bd97e631d978143b556aec9dd5e2
Ivan Tvisher <Tvisher@mail.ru>
2023-10-31 16:10:31 +0600

    add some animation fix

--- js/main.js
+++ js/main.js
@@ -29,6 +29,5 @@ window.addEventListener('load', () => {
         once: true, // whether animation should happen only once - while scrolling down
         mirror: false, // whether elements should animate out while scrolling past them
         anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
-
     });
 })
